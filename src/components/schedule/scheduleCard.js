1399df2943754c9a601fe1e3188eb02d22ce5dbd
clay-jay <youkozel@gmail.com>
2021-09-09 17:54:19 +0300

    init

---
+++ src/components/schedule/scheduleCard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { makeStyles, Grid, Typography } from "@material-ui/core"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+const useStyle = makeStyles(theme => ({
+    root: {
+
+    },
+}))
+
+export default function ScheduleCard({ schedule }){
+    const classes = useStyle()
+    
+    return{
+
+    }
+}
